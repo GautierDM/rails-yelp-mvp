e40f4c0398e5a551118e539b7addcb63953c2bd8
Gautier Delabrousse Mayoux <gautierdlb@gmail.com>
2018-11-21 16:45:20 +0100

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,12 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+
+
+
+// app/assets/javascripts/application.js
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require_tree .
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0123343546',
+    category:  'japanese'
+  },
+  {
+   name:         'Pizza East',
+   address:      '56A Shoreditch High St, London E1 6PQ',
+   phone_number: '012338546',
+   category:  'italian'
+  },
+  {
+   name:         'Le gorgeon',
+   address:      '23 av victor Hugo',
+   phone_number: '02343546',
+   category:  'french'
+  },
+  {
+   name:         'Leon de Bruxelles',
+   address:      '12 rue du laos',
+   phone_number: '0232334346',
+   category:  'belgian'
+  },
+  {
+   name:         'Diepp',
+   address:      'avenue rodin',
+   phone_number: '0323334546',
+   category:  'chinese'
+  },
+  {
+   name:         'double dragon',
+   address:      'rue oberkampf',
+   phone_number: '042204046',
+   category:  'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
